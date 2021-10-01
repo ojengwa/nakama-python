f79301eda0f3731f0af218c4ad8a1c3bd2a3797b
Laminariy <dovakin121212@gmail.com>
2021-10-01 18:52:21 +0300

    link/unlink accounts

--- nakama/account/link.py
+++ nakama/account/link.py
@@ -3,29 +3,130 @@ class Link():
     def __init__(self, client):
         self.client = client
 
-    async def email(self):
-        pass
+    async def email(self, email, password):
+        body = {
+            'email': email,
+            'password': password
+        }
 
-    async def device(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def custom(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/link/email'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
 
-    async def apple(self):
-        pass
+    async def device(self, id):
+        body = {
+            'id': id
+        }
 
-    async def facebook(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def facebook_instant_game(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/link/device'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
 
-    async def game_center(self):
-        pass
+    async def custom(self, id):
+        body = {
+            'id': id
+        }
 
-    async def google(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def steam(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/link/custom'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def apple(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/apple'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def facebook(self, token, import_friends=None):
+        params = {}
+        if import_friends is not None:
+            params['import'] = import_friends and 'true' or 'false'
+
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/facebook'
+        async with self.client._http_session.post(url_path, params=params,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def facebook_instant_game(self, signed_player_info):
+        body = {
+            'signedPlayerInfo': signed_player_info
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/facebookinstantgame'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def game_center(self, bundle_id, player_id, public_key_url, salt,
+                          signature, timestamp):
+        body = {
+            'bundleId': bundle_id,
+            'playerId': player_id,
+            'publicKeyUrl': public_key_url,
+            'salt': salt,
+            'signature': signature,
+            'timestampSeconds': timestamp
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/gamecenter'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def google(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/google'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def steam(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/link/google'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
--- nakama/account/unlink.py
+++ nakama/account/unlink.py
@@ -3,29 +3,126 @@ class Unlink():
     def __init__(self, client):
         self.client = client
 
-    async def email(self):
-        pass
+    async def email(self, email, password):
+        body = {
+            'email': email,
+            'password': password
+        }
 
-    async def device(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def custom(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/unlink/email'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
 
-    async def apple(self):
-        pass
+    async def device(self, id):
+        body = {
+            'id': id
+        }
 
-    async def facebook(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def facebook_instant_game(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/unlink/device'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
 
-    async def game_center(self):
-        pass
+    async def custom(self, id):
+        body = {
+            'id': id
+        }
 
-    async def google(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def steam(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account/unlink/custom'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def apple(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/apple'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def facebook(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/facebook'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def facebook_instant_game(self, signed_player_info):
+        body = {
+            'signedPlayerInfo': signed_player_info
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/facebookinstantgame'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def game_center(self, bundle_id, player_id, public_key_url, salt,
+                          signature, timestamp):
+        body = {
+            'bundleId': bundle_id,
+            'playerId': player_id,
+            'publicKeyUrl': public_key_url,
+            'salt': salt,
+            'signature': signature,
+            'timestampSeconds': timestamp
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/gamecenter'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def google(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/google'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def steam(self, token):
+        body = {
+            'token': token
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account/unlink/google'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
