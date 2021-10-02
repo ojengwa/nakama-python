8bc7e19f5b803267af2678add9bbd676a2e203e0
Laminariy <dovakin121212@gmail.com>
2021-10-02 15:29:46 +0300

    account

--- nakama/account/account.py
+++ nakama/account/account.py
@@ -13,10 +13,35 @@ class Account():
         self._unlink = Unlink(client)
 
     async def get(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def update(self):
-        pass
+        url_path = self.client._http_uri + '/v2/account'
+        async with self.client._http_session.get(url_path,
+                                                 headers=headers) as resp:
+            return await resp.json()
+
+    async def update(self, **kwargs):
+        body = {}
+        if 'avatar_url' in kwargs.keys():
+            body['avatarUrl'] = kwargs['avatar_url']
+        if 'display_name' in kwargs.keys():
+            body['displayName'] = kwargs['display_name']
+        if 'lang_tag' in kwargs.keys():
+            body['langTag'] = kwargs['lang_tag']
+        if 'location' in kwargs.keys():
+            body['location'] = kwargs['location']
+        if 'timezone' in kwargs.keys():
+            body['timezone'] = kwargs['timezone']
+        if 'username' in kwargs.keys():
+            body['username'] = kwargs['username']
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/account'
+        async with self.client._http_session.put(url_path,
+                                                 headers=headers,
+                                                 json=body) as resp:
+            return await resp.json()
 
     @property
     def authenticate(self):
