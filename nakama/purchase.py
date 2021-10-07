29d6a3d6bf7aadcd46db76fd5867a46000dad1a8
Laminariy <dovakin121212@gmail.com>
2021-10-07 19:01:09 +0300

    purchase

--- nakama/purchase.py
+++ nakama/purchase.py
@@ -3,11 +3,42 @@ class Purchase():
     def __init__(self, client):
         self.client = client
 
-    async def validate_apple(self):
-        pass
+    async def validate_apple(self, receipt):
+        body = {
+            'receipt': receipt
+        }
 
-    async def validate_google(self):
-        pass
+        headers = self.client.session.auth_header
 
-    async def validate_huawei(self):
-        pass
+        url_path = self.client._http_uri + '/v2/iap/purchase/apple'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def validate_google(self, purchase):
+        body = {
+            'purchase': purchase
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/iap/purchase/google'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
+
+    async def validate_huawei(self, purchase, signature):
+        body = {
+            'purchase': purchase,
+            'signature': signature
+        }
+
+        headers = self.client.session.auth_header
+
+        url_path = self.client._http_uri + '/v2/iap/purchase/huawei'
+        async with self.client._http_session.post(url_path,
+                                                  headers=headers,
+                                                  json=body) as resp:
+            return await resp.json()
